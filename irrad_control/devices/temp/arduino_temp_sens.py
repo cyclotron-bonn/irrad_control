9471532fe1f6575e90da018c690abbeac57cc66f
leloup314 <wolf@physik.uni-bonn.de>
2020-04-18 00:58:44 +0200

    MAINT: raise error if ports are not free

--- irrad_control/devices/temp/arduino_temp_sens.py
+++ irrad_control/devices/temp/arduino_temp_sens.py
@@ -9,40 +9,27 @@ class ArduinoTempSens(object):
     # Delimiter which separates the sensor numbers in the command string which is send via serial
     cmd_delimiter = 'T'
 
-    def __init__(self, port="/dev/ttyUSB0", baudrate=9600, timeout=5, ntc_lim=(-55, 125)):
+    def __init__(self, port="/dev/ttyUSB0", baudrate=115200, timeout=5, ntc_lim=(-55, 125)):
         super(ArduinoTempSens, self).__init__()
 
         self.ntc_lim = ntc_lim  # Store temperature limits of NTC thermistor
 
         # Make nice serial interface
-        try:
-            self.interface = serial.Serial(port=port, baudrate=baudrate, timeout=timeout)
-            time.sleep(2)  # Sleep to allow Arduino to reboot caused by serial connection
+        self.interface = serial.Serial(port=port, baudrate=baudrate, timeout=timeout)
+        time.sleep(2)  # Sleep to allow Arduino to reboot caused by serial connection
 
-        # Catch exception and log instead of raising error because we use this in a multi-threaded environment
-        except serial.SerialException:
-            logging.error("Could not connect to port {}. Maybe it is used by another process?".format(port))
-            self.interface = None
+        # Check connection by writing invalid data and receiving answer
+        self.interface.write('{}100'.format(self.cmd_delimiter).encode())
+        test_res = float(self.interface.readline().strip())
 
-        # Check connection; if we have one, this resets the Arduino and causes reboot. We must wait before we write to serial
-        if self.interface is not None:
-
-            # Check connection by writing invalid data and receiving answer
-            self.interface.write('{}100'.format(self.cmd_delimiter).encode())
-            test_res = float(self.interface.readline().strip())
-
-            if test_res == 999.:
-                logging.debug("Serial connection to Arduino temperature sensor established.")
-            else:
-                logging.error("No reply on serial connection to Arduino temperature sensor.")
+        if test_res == 999.:
+            logging.debug("Serial connection to Arduino temperature sensor established.")
+        else:
+            logging.error("No reply on serial connection to Arduino temperature sensor.")
 
     def get_temp(self, sensor):
         """Gets temperature of sensor where 0 <= sensor <= 7 is the physical pin number of the sensor on
         the Arduino analog pin. Can also be a list of ints."""
-
-        if self.interface is None:
-            logging.warning("Serial interface to Arduino temperature sensor not established. Retry!")
-            return
 
         # Make int sensors to list
         sensor = sensor if isinstance(sensor, list) else [sensor]
