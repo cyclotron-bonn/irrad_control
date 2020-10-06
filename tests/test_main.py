91412f727d91b0851d6e4b45cf4b0bce546403bf
leloup314 <wolf@physik.uni-bonn.de>
2020-10-06 03:24:41 +0200

    PRJ: try fix Travis

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,9 +3,49 @@ import logging
 import unittest
 import time
 
-from PyQt5 import QtWidgets
+from PyQt5 import QtWidgets, QtCore
 
 from irrad_control.main import IrradControlWin
+
+
+class TestIrradWin(IrradControlWin):
+    """Test class that does not try to access the screen"""
+
+    def _init_ui(self):
+        """
+        Initializes the user interface and displays "Hello"-message
+        """
+
+        # Create main layout
+        self.main_widget = QtWidgets.QWidget()
+        self.main_layout = QtWidgets.QVBoxLayout(self.main_widget)
+        self.setCentralWidget(self.main_widget)
+
+        # Add QTabWidget for tab_widget
+        self.tabs = QtWidgets.QTabWidget()
+
+        # Main splitter
+        self.main_splitter = QtWidgets.QSplitter()
+        self.main_splitter.setOrientation(QtCore.Qt.Vertical)
+        self.main_splitter.setChildrenCollapsible(False)
+
+        # Sub splitter for log and displaying raw data as it comes in
+        self.sub_splitter = QtWidgets.QSplitter()
+        self.sub_splitter.setOrientation(QtCore.Qt.Horizontal)
+        self.sub_splitter.setChildrenCollapsible(False)
+
+        # Add to main layout
+        self.main_splitter.addWidget(self.tabs)
+        self.main_splitter.addWidget(self.sub_splitter)
+        self.main_layout.addWidget(self.main_splitter)
+
+        # Init widgets and add to main windowScatterPlotItem
+        self._init_menu()
+        self._init_tabs()
+        self._init_log_dock()
+
+        self.sub_splitter.setSizes([int(1. / 3. * self.width()), int(2. / 3. * self.width())])
+        self.main_splitter.setSizes([int(0.8 * self.height()), int(0.2 * self.height())])
 
 
 class TestMain(unittest.TestCase):
@@ -17,7 +57,7 @@ class TestMain(unittest.TestCase):
         cls.test_app = QtWidgets.QApplication(sys.argv)
 
         # Create complete window which can be accessed after launch
-        cls.irrad_window = IrradControlWin()
+        cls.irrad_window = TestIrradWin()
 
         time.sleep(10)  # Workaround for threaded launch
 
