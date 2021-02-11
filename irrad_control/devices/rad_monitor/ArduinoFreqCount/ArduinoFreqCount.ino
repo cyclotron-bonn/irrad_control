a90c40b33e365d572fc6fceb048cbb31c49c53b7
christian235 <ch.wer98@gmail.com>
2021-02-11 14:55:29 +0100

    @leloup314 Now the program is completly working!

--- irrad_control/devices/rad_monitor/ArduinoFreqCount/ArduinoFreqCount.ino
+++ irrad_control/devices/rad_monitor/ArduinoFreqCount/ArduinoFreqCount.ino
@@ -6,43 +6,62 @@
 
 #include <FreqCount.h>
 
+long samplingtime=1000;
+long value=0;
+
 void setup() {
   Serial.begin(9600);
-  FreqCount.begin(1000);
+  //st stands for smaplingtime and it starts with 1000
+  FreqCount.begin(samplingtime);  
 }
 
 void loop() {
+  long st;
   char setting = {'s'};
   char getting = {'g'};
-  char samplingtime = {'t'};
-  long st;
-  char cmd[6];
-  int x;
+  char time = {'t'};
+  char cmd[7];
   int k;
-  int i;
+  int i=0;
+  int l = 2;
+  int number[] = {};
   
   if(Serial.available()){
-      for(k=0; k<=6; k++){
+      for(k=0; k<=8; k++){
         cmd[k] = Serial.read();
         delay(100);
       }
       if(cmd[0] == setting){
-        Serial.print('1');
-        if(cmd[1] == samplingtime){
-          Serial.println('3');
+        if(cmd[1] == time){
+          while(cmd[l] == '0' || cmd[l] == '1' || cmd[l] == '2' || cmd[l] == '3' || cmd[l] == '4' || cmd[l] == '5' || cmd[l] == '6' || cmd[l] == '7' || cmd[l] == '8' || cmd[l] == '9'){
+            number[i] = cmd[l] - '0';
+            i = i + 1;
+            l = l + 1;
+          }
+          Serial.println(number[0]);
+          //Serial.print(number[1]);
+          //Serial.println(number[2]);
+          //delay(4000);
+          samplingtime = 0;
+          for(int j=0; j < i; j++){
+            samplingtime = samplingtime*10;
+            samplingtime = samplingtime + number[j];
+          }
+          Serial.println(samplingtime);
+          FreqCount.begin(samplingtime);
         }
       }
       else if(cmd[0] == getting){
-        Serial.print('2');
-        if(cmd[1] == samplingtime){
-          Serial.print('3');
+        if(cmd[1] == time){
+          Serial.println(samplingtime);
         }
       }
   }
- // if(FreqCount.available()){
- //   unsigned long count = FreqCount.read();
- //   Serial.println(count);
- // }
+  if(FreqCount.available()){
+    FreqCount.begin(samplingtime);
+    unsigned long count = FreqCount.read();
+    Serial.println(count);
+  }
 }
 
 
