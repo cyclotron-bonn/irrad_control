98c948a59e38be45f6a8704c294b904634b3f2e9
leloup314 <s6plwolf@uni-bonn.de>
2021-05-06 14:37:36 +0200

    FIX: fix correct access of daq board config

--- irrad_control/devices/readout/daq_board.py
+++ irrad_control/devices/readout/daq_board.py
@@ -16,8 +16,6 @@ class IrradDAQBoard(object):
         # Initialize the interface to the board via I2C
         self._intf = TCA9555(address=address)
 
-        # Store the board config
-        self.config = DAQ_BOARD_CONFIG
         self.version = version
 
         # Related to temperature channel cycling
@@ -31,89 +29,91 @@ class IrradDAQBoard(object):
     def restore_defaults(self):
 
         # Set the direction (in or output) of the pins
-        self._intf.set_direction(direction=0, bits=self.config[self.version]['defaults']['output'])
-        self._intf.set_direction(direction=1, bits=self.config[self.version]['defaults']['input'])
+        self._intf.set_direction(direction=0, bits=DAQ_BOARD_CONFIG['version'][self.version]['defaults']['output'])
+        self._intf.set_direction(direction=1, bits=DAQ_BOARD_CONFIG['version'][self.version]['defaults']['input'])
 
         # Set the input current scale IFS
-        self.set_ifs(group='sem', ifs=self.config[self.version]['defaults']['sem_ifs'])
+        self.set_ifs(group='sem', ifs=DAQ_BOARD_CONFIG['version'][self.version]['defaults']['sem_ifs'])
         # Set the input current scale
-        self.set_ifs(group='ch12', ifs=self.config[self.version]['defaults']['ch12_ifs'])
+        self.set_ifs(group='ch12', ifs=DAQ_BOARD_CONFIG['version'][self.version]['defaults']['ch12_ifs'])
 
-        self.set_temp_channel(channel=self.config[self.version]['defaults']['temp_ch'])
+        self.set_temp_channel(channel=DAQ_BOARD_CONFIG['version'][self.version]['defaults']['temp_ch'])
 
     @property
     def jumper_scale(self):
-        return self.config['jumper_scale']
+        # FIXME
+        return DAQ_BOARD_CONFIG['common']['jumper_scale']
 
     @jumper_scale.setter
     def jumper_scale(self, js):
         if js not in (1, 10):
             raise ValueError('The input jumper scales the full-scale current range (IFS) either by 1 or 10.')
-        self.config['jumper_scale'] = js
+        # FIXME this changes value in global constant config
+        DAQ_BOARD_CONFIG['common']['jumper_scale'] = js
 
     @property
     def gpio_value(self):
-        return self._intf.int_from_bits(bits=self.config[self.version]['pins']['gpio'])
+        return self._intf.int_from_bits(bits=DAQ_BOARD_CONFIG['version'][self.version]['pins']['gpio'])
 
     @gpio_value.setter
     def gpio_value(self, val):
-        self._intf.int_to_bits(bits=self.config[self.version]['pins']['gpio'], val=val)
+        self._intf.int_to_bits(bits=DAQ_BOARD_CONFIG['version'][self.version]['pins']['gpio'], val=val)
 
     @property
     def gpio_direction(self):
-        return [self._intf.format_config()['config'][p] for p in self.config[self.version]['pins']['gpio']]
+        return [self._intf.format_config()['config'][p] for p in DAQ_BOARD_CONFIG['version'][self.version]['pins']['gpio']]
 
     @gpio_direction.setter
     def gpio_direction(self, direction):
-        self._intf.set_direction(bits=self.config[self.version]['pins']['gpio'], direction=direction)
+        self._intf.set_direction(bits=DAQ_BOARD_CONFIG['version'][self.version]['pins']['gpio'], direction=direction)
 
     def set_mux_value(self, group, val):
 
-        self._intf.int_to_bits(self.config[self.version]['pins'][group], val=val)
+        self._intf.int_to_bits(DAQ_BOARD_CONFIG['version'][self.version]['pins'][group], val=val)
 
     def get_mux_value(self, group):
 
-        return self._intf.int_from_bits(bits=self.config[self.version]['pins'][group])
+        return self._intf.int_from_bits(bits=DAQ_BOARD_CONFIG['version'][self.version]['pins'][group])
 
     def get_temp_channel(self, cached=False):
 
         if self.temp_channel is None or not cached:
-            self.temp_channel = self._intf.int_from_bits(bits=self.config[self.version]['pins']['temp'])
+            self.temp_channel = self._intf.int_from_bits(bits=DAQ_BOARD_CONFIG['version'][self.version]['pins']['temp'])
 
         return self.temp_channel
 
     def set_temp_channel(self, channel):
 
-        self._intf.int_to_bits(bits=self.config[self.version]['pins']['temp'], val=channel)
+        self._intf.int_to_bits(bits=DAQ_BOARD_CONFIG['version'][self.version]['pins']['temp'], val=channel)
 
         self.temp_channel = channel
 
     def set_ifs(self, group, ifs):
+        # FIXME
+        ifs_idx = DAQ_BOARD_CONFIG['common']['ifs_scales'].index(ifs / DAQ_BOARD_CONFIG['common']['jumper_scale'])
 
-        ifs_idx = self.config['ifs_scales'].index(ifs / self.config['jumper_scale'])
-
-        self._intf.int_to_bits(self.config[self.version]['pins'][group], val=ifs_idx)
+        self._intf.int_to_bits(DAQ_BOARD_CONFIG['version'][self.version]['pins'][group], val=ifs_idx)
 
     def get_ifs(self, group):
 
-        ifs_idx = self._intf.int_from_bits(bits=self.config[self.version]['pins'][group])
-
-        return self.config['ifs_scales'][ifs_idx] * self.config['jumper_scale']
+        ifs_idx = self._intf.int_from_bits(bits=DAQ_BOARD_CONFIG['version'][self.version]['pins'][group])
+        # FIXME
+        return DAQ_BOARD_CONFIG['common']['ifs_scales'][ifs_idx] * DAQ_BOARD_CONFIG['common']['jumper_scale']
 
     def get_ifs_label(self, group):
 
-        ifs_idx = self._intf.int_from_bits(bits=self.config[self.version]['pins'][group])
-
-        return self.config['ifs_labels'][ifs_idx] if self.config['jumper_scale'] == 1 else self.config['ifs_labels_10'][ifs_idx]
+        ifs_idx = self._intf.int_from_bits(bits=DAQ_BOARD_CONFIG['version'][self.version]['pins'][group])
+        # FIXME
+        return DAQ_BOARD_CONFIG['common']['ifs_labels'][ifs_idx] if DAQ_BOARD_CONFIG['common']['jumper_scale'] == 1 else DAQ_BOARD_CONFIG['common']['ifs_labels_10'][ifs_idx]
 
     def _check_and_map_gpio(self, pins):
 
         pins = [pins] if isinstance(pins, int) else pins
 
-        if any(not 0 <= p < len(self.config[self.version]['pins']['gpio']) for p in pins):
-            raise IndexError("GPIO pins are indexed from {} to {}".format(0, len(self.config[self.version]['pins']['gpio']) - 1))
+        if any(not 0 <= p < len(DAQ_BOARD_CONFIG['version'][self.version]['pins']['gpio']) for p in pins):
+            raise IndexError("GPIO pins are indexed from {} to {}".format(0, len(DAQ_BOARD_CONFIG['version'][self.version]['pins']['gpio']) - 1))
 
-        return [self.config[self.version]['pins']['gpio'][p] for p in pins]
+        return [DAQ_BOARD_CONFIG['version'][self.version]['pins']['gpio'][p] for p in pins]
 
     def set_gpio_value(self, pins, value):
 
