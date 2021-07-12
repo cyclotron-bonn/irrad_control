9dfd5dfb8a370e71cee93b0e5aae420bebe0b9c3
leloup314 <wolf@physik.uni-bonn.de>
2021-07-13 01:46:22 +0200

    MAINT: add decimals to constants; add to init

--- irrad_control/analysis/constants.py
+++ irrad_control/analysis/constants.py
@@ -1,7 +1,7 @@
 """Collection of constants needed"""
 
 # Elementary charge
-elementary_charge = 1.602e-19  # Coulomb
+elementary_charge = 1.60217733e-19  # Coulomb
 
 # Stopping power of 12.72 MeV protons in Si
 p_stop_Si = 28.74  # MeV cm^2 / g
