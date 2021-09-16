ac57141cbf5de837780058b7d8c6f177e653962f
leloup314 <wolf@physik.uni-bonn.de>
2021-09-16 17:14:55 +0200

    ENH: enhance loading irrad data

--- irrad_control/analysis/utils.py
+++ irrad_control/analysis/utils.py
@@ -1,10 +1,20 @@
 import tables as tb
+import collections.abc
 
 # Package imports
 from irrad_control.utils.tools import load_yaml
 
 
-def load_irrad_data(data_file, config_file, specify_entries=None, substract_raw_offset=True):
+def update_dict(d, u):
+    for k, v in u.items():
+        if isinstance(v, collections.abc.Mapping):
+            d[k] = update_dict(d.get(k, {}), v)
+        else:
+            d[k] = v
+    return d
+
+
+def load_irrad_data(data_file, config_file, specify_entries=None, subtract_raw_offset=True):
     """
     Function that reads the output files of an irradiation and returns generated data and configuration
 
@@ -16,7 +26,7 @@ def load_irrad_data(data_file, config_file, specify_entries=None, substract_raw_
         Path to the config file of respective irradiation
     specify_entries: list, str, None
         Name or iterable of names of entries to load. I None, all entries are loaded
-    substract_raw_offset: bool
+    subtract_raw_offset: bool
         Whether to subtract the offset from the raw data
 
     Returns
@@ -33,27 +43,38 @@ def load_irrad_data(data_file, config_file, specify_entries=None, substract_raw_
     # Check if we only want to load one field or all
     entries_to_load = [specify_entries] if isinstance(specify_entries, str) else specify_entries
 
-    # Loop over DAQ servers and load data
-    for server in irrad_config['server']:
+    # Open actual HDF5 file
+    with tb.open_file(data_file) as dfile:
 
-        # Open actual HDF5 file
-        with tb.open_file(data_file) as dfile:
+        # Loop over DAQ servers and load data
+        for server in irrad_config['server']:
 
             # Extract respective servers data
             server_name = irrad_config['server'][server]['name']
-            server_data = dfile.root[server_name]
-            available_nodes = tuple(server_data._v_children.keys())
-            entries_to_load = available_nodes if entries_to_load is None else entries_to_load
 
             # Hold server data in dict
             irrad_data[server_name] = {}
 
-            # Read entries
-            for entry in entries_to_load:
-                try:
-                    irrad_data[server_name][entry] = server_data[entry].read()
-                except IndexError:
-                    raise IndexError("No child node with name '{}' exists. Existing children: {}".format(entry, ', '.join(available_nodes)))
+            # Read leaves i.e. Arrays and Tables
+            for leaf in dfile.walk_nodes("/", classname='Leaf'):
+
+                # Get all the nested levels; Ignore first 2 elements since they are empty (0) and server_name (1)
+                data_depth = leaf._v_pathname.split('/')[2:]
+
+                # Skip this leaf if it belongs to a top-level node that is not specified to be read
+                if specify_entries:
+                    if data_depth[0] not in specify_entries:
+                        continue
+
+                # Make data dict with all nested levels needed
+                nested_data = {}
+                for i, d in enumerate(reversed(data_depth)):
+                    nested_data = {d: nested_data}
+                    if i == 0:
+                        nested_data[d] = leaf.read()
+
+                # Update the respective data in the return dict for this server
+                update_dict(irrad_data[server_name], nested_data)
 
             # Subtract offsets of raw data
             if substract_raw_offset and all(e in irrad_data[server_name] for e in ('Raw', 'RawOffset')):
