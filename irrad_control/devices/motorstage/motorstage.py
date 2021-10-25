c2cac5bbdbf4c5064cf3bf5b6004f28666345fc5
leloup314 <wolf@physik.uni-bonn.de>
2021-10-25 19:51:31 +0200

    ENH: allow motorstage instance to read from device config

--- irrad_control/devices/motorstage/motorstage.py
+++ irrad_control/devices/motorstage/motorstage.py
@@ -1,12 +1,21 @@
 from .zaber import ZaberStepAxis, ZaberMultiAxis
 from .item import ItemLinearStage
+from irrad_control.devices import DEVICES_CONFIG
 
 
 class ScanStage(ZaberMultiAxis):
     """
     2-dimensional (XY-plane w.r.t beam axis (Z)) motorstage configuration used to scan DUTs through the beam.
     """
-    pass
+    def __init__(self, **kwargs):
+
+        # Get init kwargs from config
+        init = DEVICES_CONFIG[type(self).__name__]['init']
+
+        # Update init kwargs if *kwargs* are given
+        init.update(kwargs)
+
+        super(ScanStage, self).__init__(**init)
 
 
 class SetupTableStage(ZaberStepAxis):
@@ -14,7 +23,15 @@ class SetupTableStage(ZaberStepAxis):
     1-dimensional (Z-axis aka beam axis) motorstage which moves the setup table, on which the ScanStage is mounted,
     along the beam axis.
     """
-    pass
+    def __init__(self, **kwargs):
+
+        # Get init kwargs from config
+        init = DEVICES_CONFIG[type(self).__name__]['init']
+
+        # Update init kwargs if *kwargs* are given
+        init.update(kwargs)
+
+        super(SetupTableStage, self).__init__(**init)
 
 
 class ExternalCupStage(ItemLinearStage):
@@ -22,4 +39,13 @@ class ExternalCupStage(ItemLinearStage):
     1-dimensional (Y-axis w.r.t beam axis (Z)) motorstage, carrying an external FaradayCup (FC) and
     fluorescence screen combination, allowing to move into and out of the beam directly behind extraction.
     """
-    pass
+
+    def __init__(self, **kwargs):
+
+        # Get init kwargs from config
+        init = DEVICES_CONFIG[type(self).__name__]['init']
+
+        # Update init kwargs if *kwargs* are given
+        init.update(kwargs)
+
+        super(ExternalCupStage, self).__init__(**init)
