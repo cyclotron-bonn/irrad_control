06b545eed378289c6495f57cfc0fbaf7f8843249
bedakno <s6beknop@uni-bonn.de>
2022-02-13 00:03:32 +0100

    ENHC: ArdRO now works

--- irrad_control/devices/arduino/serial_to_i2c/ArdRO.py
+++ irrad_control/devices/arduino/serial_to_i2c/ArdRO.py
@@ -1,18 +1,18 @@
-from irrad_control.devices.arduino.serial_to_i2c import ArdSer
+from irrad_control.devices.arduino import ArdSer
 
-class ArdRO(ArdSer):
+class ArdRO(ArdSer.ArdSer):
     def __init__(self, port, address=0x20, baudrate=115200, timeout = 1.):
-        self.intf = super().__init__(port=port, baudrate=baudrate, timeout=timeout)
-        cmd = self.intf.create_command('A',32)
-        self.intf.query(cmd)
+        super().__init__(port=port, baudrate=baudrate, timeout=timeout)
+        cmd = super().create_command('A',32)
+        super().query(cmd)
 
     def read_data(self, reg):
         """
         reads data from a given register
         """
         #transmit data to get the value from a certain register reg
-        msg = self.intf.create_command('R', reg)
-        ans = self.intf.query(msg)
+        msg = self._intf.create_command('R', reg)
+        ans = self._intf.query(msg)
         return int(ans)
 
     def write_data(self, reg, data):
@@ -20,8 +20,8 @@ class ArdRO(ArdSer):
         writes data to a given register
         """
         #transmit data to set the value val of a certain register reg
-        msg = self.intf.cre_cmd('W', reg, data)
-        self.intf.query(msg)
+        msg = self._intf.cre_cmd('W', reg, data)
+        self._intf.query(msg)
     
     def check_i2c_con(self):
         """checks the i2c connection from arduino to bus device
@@ -29,8 +29,8 @@ class ArdRO(ArdSer):
             RuntimeError: the arduino should return a 0 if the test was successful,
                           anything else is an error
         """
-        cmd = self.intf.create_command('T')
-        check = int(self.intf.query(cmd))
+        cmd = self.create_command('T')
+        check = int(self.query(cmd))
         if check != 0:
             raise RuntimeError("I2C connection to bus device unsuccessful")
     
@@ -40,5 +40,5 @@ class ArdRO(ArdSer):
         Args:
             add (int): new address
         """
-        cmd = self.intf.create_command('A', add)
-        self.intf.query(cmd)
+        cmd = self._intf.create_command('A', add)
+        self._intf.query(cmd)
