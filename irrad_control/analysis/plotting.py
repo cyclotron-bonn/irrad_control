aa99a575ff6313912d6c12e63b9f18466c42b1e6
leloup314 <wolf@physik.uni-bonn.de>
2022-04-26 15:25:02 +0200

    ENH: add generic figure plotting function

--- irrad_control/analysis/plotting.py
+++ irrad_control/analysis/plotting.py
@@ -93,3 +93,36 @@ def plot_damage_map_contourf(damage_map, map_centers_x, map_centers_y, cmap='vir
     _apply_labels_damage_plots(ax=ax, cbar=cbar, damage_map=damage_map, **damage_label_kwargs)
 
     return fig, ax
+
+
+def plot_generic_fig(plot_data, fit_data=None, hist_data=None, **sp_kwargs):
+    
+    fig, ax = plt.subplots(**sp_kwargs)
+    
+    # Make figure and axis
+    ax.set_title(plot_data['title'])
+    ax.set_xlabel(plot_data['xlabel'])
+    ax.set_ylabel(plot_data['ylabel'])
+    if fit_data:
+        ax.plot(fit_data['xdata'], fit_data['func'](*fit_data['fit_args']), fit_data['fmt'], label=fit_data['label'], zorder=10)
+    if hist_data:
+        if isinstance(hist_data['bins'], (int, str)):
+            if hist_data['bins'] == 'stat':
+                n, s = np.mean(plot_data['xdata']), np.std(plot_data['xdata'])
+                binwidth = 6 * s / 100.
+                bins = np.arange(n-3*s, n+3*s + binwidth, binwidth)
+            else:
+                bins = hist_data['bins']
+
+            _, _, _ = ax.hist(plot_data['xdata'], bins=bins, label=plot_data['label'])
+        elif len(hist_data['bins']) == 2:
+            _, _, _, im = ax.hist2d(plot_data['xdata'], plot_data['ydata'], bins=hist_data['bins'],norm=hist_data['norm'], cmap=hist_data['cmap'], cmin=1, label=plot_data['label'])
+            plt.colorbar(im)
+        else:
+            raise ValueError('bins must be 2D iterable of intsd or int')
+    else:
+        ax.plot(plot_data['xdata'], plot_data['ydata'], plot_data['fmt'], label=plot_data['label'])
+    ax.grid()
+    ax.legend(loc='upper left')
+    
+    return fig, ax
