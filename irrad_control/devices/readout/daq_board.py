8ceb65e7e7a026b80b486971e8d64f56525536cb
leloup314 <s6plwolf@uni-bonn.de>
2022-09-06 18:41:48 +0200

    FIX: wrong instance method call

--- irrad_control/devices/readout/daq_board.py
+++ irrad_control/devices/readout/daq_board.py
@@ -93,8 +93,8 @@ class IrradDAQBoard(object):
     def set_ntc_channel(self, channel):
 
         # In case of cycling channels
-        if self.is_cycling_temp_channels():
-            self.stop_cycle_temp_channels()
+        if self.is_cycling_ntc_channels():
+            self.stop_cycle_ntc_channels()
 
         self._intf.int_to_bits(bits=DAQ_BOARD_CONFIG['version'][self.version]['pins']['ntc'], val=channel)
 
