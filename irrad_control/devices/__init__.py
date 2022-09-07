8bc9e639e5aa829273311de0d0a363be8d1249d3
leloup314 <s6plwolf@uni-bonn.de>
2022-09-07 18:35:26 +0200

    FIX: load device configs

--- irrad_control/devices/__init__.py
+++ irrad_control/devices/__init__.py
@@ -3,28 +3,27 @@ from os.path import isfile
 from irrad_control.utils.tools import location, load_yaml, make_path
 
 
+DEVICES_CONFIG = load_yaml(make_path(location(__file__), 'devices_config.yaml'))
+
+
 # Check if config files need to be loaded for device inits
 def load_device_init_configs():
     # Loop over devices in device config dict
-    for dev, init in DEVICES_CONFIG.items():
+    for _, init in DEVICES_CONFIG.items():
 
         # Config is in init
-        if 'config' in init:
+        if 'config' in init['init']:
 
             # Check if config is already a dict or needs to be loaded from yaml
-            if not isinstance(init['config'], dict):
+            if not isinstance(init['init']['config'], dict):
                 # Check if config file exists and overwrite, otherwise config is None
-                if isfile(init['config']):
+                if isfile(init['init']['config']):
                     try:
-                        config_file = str(init['config'])
-                        init['config'] = load_yaml(config_file)
-                        init['config']['filename'] = config_file
+                        config_file = str(init['init']['config'])
+                        init['init']['config'] = load_yaml(config_file)
+                        init['init']['config']['filename'] = config_file
                     except FileNotFoundError:
-                        init['config'] = None
+                        init['init']['config'] = None
                         logging.warning("Config file {} could not be found!".format(config_file))
-
-
-DEVICES_CONFIG = load_yaml(make_path(location(__file__), 'devices_config.yaml'))
-
 
 load_device_init_configs()
