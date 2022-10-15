663e7c935c2d29b6f5e4d69831b16531af6e07e5
leloup314 <wolf@physik.uni-bonn.de>
2022-10-16 00:13:20 +0200

    MAINT: adapt

--- irrad_control/ions/proton/__init__.py
+++ irrad_control/ions/proton/__init__.py
@@ -1,4 +1,5 @@
+import os
 from irrad_control.ions import IrradIon
 
 
-proton = IrradIon(name='proton', n_charge=1, n_nucleon=1)
+proton = IrradIon(name='proton', n_charge=1, n_nucleon=1, data_path=os.path.dirname(__file__))
