ffda34ae3f124720a31bedff29732ed11081198d
leloup314 <wolf@physik.uni-bonn.de>
2023-04-17 20:02:08 +0200

    ENH: improve plotting in general; refactor plotting for beam anyalsis

--- irrad_control/analysis/beam.py
+++ irrad_control/analysis/beam.py
@@ -1,29 +1,38 @@
-import logging
 import numpy as np
 from irrad_control.analysis import plotting, constants
 
 
 def main(data, config=None):
-    server = config['name']
-    #get data while scanning
+
     figs = []
-    nano = np.array(constants.nano)
-    beam_current_nA = data[server]['Beam']['beam_current']/nano #beam current in nA
-    #Beam Current over time
-    logging.info("Generating plots...")
-    fig, _ = plotting.plot_beam_current(timestamps=data[server]['Beam']['timestamp'], beam_currents=beam_current_nA)
-    
+    server = config['name']
+
+    beam_current = data[server]['Beam']['beam_current'] / constants.nano
+
+    # Beam current over time
+    fig, _ = plotting.plot_beam_current(timestamps=data[server]['Beam']['timestamp'],
+                                        beam_current=beam_current)
     figs.append(fig)
-    
-    #Histogram of beam_current
-    fig, _ = plotting.plot_beam_current_hist(beam_currents=beam_current_nA,
-                                             start=data[server]['Scan']['row_start_timestamp'][0],
-                                             end=data[server]['Scan']['row_start_timestamp'][-1])
-    
+
+    # Beam current histogram
+    plot_data = {
+        'xdata': beam_current,
+        'xlabel': 'Beam current / nA',
+        'ylabel': '#',
+        'label': "Beam current over {}".format(plotting._calc_duration(start=data[server]['Beam']['timestamp'][0],
+                                                                       end=data[server]['Beam']['timestamp'][-1],
+                                                                       as_str=True)),
+        'title': "Beam current distribution",
+        'fmt': 'C0'
+    }
+    plot_data['label'] += ":\n    ({:.2f}{}{:.2f}) nA".format(beam_current.mean(), u'\u00b1', beam_current.std())
+
+    fig, _ = plotting.plot_generic_fig(plot_data=plot_data, hist_data={'bins': 'stat'})
     figs.append(fig)
-    #Histogram of beam current diviation
-    fig, _ = plotting.plot_beam_deviation(horizontal_deviation=data[server]['Beam']['horizontal_beam_position'],
-                                          vertical_deviation=data[server]['Beam']['vertical_beam_position'])
+
+    # Relative position of beam-mean wrt the beam pipe center
+    fig, _ = plotting.plot_relative_beam_position(horizontal_pos=data[server]['Beam']['horizontal_beam_position'],
+                                                  vertical_pos=data[server]['Beam']['vertical_beam_position'])
     figs.append(fig)
-    logging.info("Finished plotting.")
+
     return figs
--- irrad_control/analysis/plotting.py
+++ irrad_control/analysis/plotting.py
@@ -5,15 +5,20 @@ import irrad_control.analysis.constants as irrad_consts
 
 from datetime import datetime
 from matplotlib.colors import LogNorm
+from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 from irrad_control.analysis.formulas import lin_odr, tid_per_scan
 
-# Set matplotlib rcParams
+
+# Set matplotlib rcParams to steer appearance of irrad_analysis plots
 plt.rcParams.update({
     'font.size': 11,  # default 10
     'figure.figsize': [8, 6],  # default [6.4, 4.8]
     'grid.alpha': 0.75,  # default 1.0
-    'figure.max_open_warning': 0  # default 20; disable matplotlib figure number warning; expect people to have more than 2 GB of RAM
+    'figure.max_open_warning': 0,  # default 20; disable matplotlib figure number warning; expect people to have more than 2 GB of RAM
+    'path.simplify': True,  # default False: removes vertices that have no impact on plot visualization for the sake of better rendering performance
+    'axes.grid' : True,
+    'image.cmap': 'plasma'
     }
 )
 
@@ -61,8 +66,20 @@ def _make_cbar(fig, damage_map, damage, ion_name, rel_error_lims=None):
         cbar_rel_axis.ticklabel_format(axis='y', useOffset=False, style='plain')
         cbar_rel_axis.set_ylim(rel_error_lims)
 
+def _calc_duration(start, end, as_str=False):
 
-def plot_damage_error_3d(damage_map, error_map, map_centers_x, map_centers_y, view_angle=(25, -115), cmap='viridis', contour=False, **damage_label_kwargs):
+    duration = end - start
+    days = duration / (24 * 3600)
+    hours = (days % 1) * 24
+    minutes = (hours % 1) * 60
+    seconds = (minutes % 1) * 60
+    
+    # Return tuple in full days, hours, minutes and seconds
+    res = tuple(int(x) for x in [days, hours, minutes, seconds])
+    
+    return res if not as_str else ", ".join(f"{a[0]}{a[1]}" for a in zip(res, 'dhms') if a[0])
+
+def plot_damage_error_3d(damage_map, error_map, map_centers_x, map_centers_y, view_angle=(25, -115), contour=False, **damage_label_kwargs):
 
     # Make figure
     fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
@@ -71,7 +88,7 @@ def plot_damage_error_3d(damage_map, error_map, map_centers_x, map_centers_y, vi
     mesh_x, mesh_y = np.meshgrid(map_centers_x, map_centers_y)
 
     # plot surface
-    surface_3d = ax.plot_surface(mesh_x, mesh_y, error_map, antialiased=True, cmap=cmap)
+    surface_3d = ax.plot_surface(mesh_x, mesh_y, error_map, antialiased=True)
     
     # Adjust angle
     ax.view_init(*view_angle)
@@ -89,7 +106,7 @@ def plot_damage_error_3d(damage_map, error_map, map_centers_x, map_centers_y, vi
 
 
 
-def plot_damage_map_3d(damage_map, map_centers_x, map_centers_y, view_angle=(25, -115), cmap='viridis', contour=False, **damage_label_kwargs):
+def plot_damage_map_3d(damage_map, map_centers_x, map_centers_y, view_angle=(25, -115), contour=False, **damage_label_kwargs):
 
     # Make figure
     fig, ax = plt.subplots(figsize=(8, 6), subplot_kw={"projection": "3d"})
@@ -98,11 +115,11 @@ def plot_damage_map_3d(damage_map, map_centers_x, map_centers_y, view_angle=(25,
     mesh_x, mesh_y = np.meshgrid(map_centers_x, map_centers_y)
 
     # plot surface
-    surface_3d = ax.plot_surface(mesh_x, mesh_y, damage_map, antialiased=True, cmap=cmap)
+    surface_3d = ax.plot_surface(mesh_x, mesh_y, damage_map, antialiased=True)
 
     # Plot contour
     if contour:
-        contour_2d = ax.contourf(mesh_x, mesh_y, damage_map, zdir='z', offset=-0.05*damage_map.max(), cmap=cmap)
+        contour_2d = ax.contourf(mesh_x, mesh_y, damage_map, zdir='z', offset=-0.05*damage_map.max())
         ax.set_zlim(-0.05*damage_map.max(), damage_map.max())
         
     # Adjust angle
@@ -117,7 +134,7 @@ def plot_damage_map_3d(damage_map, map_centers_x, map_centers_y, view_angle=(25,
     return fig, ax
 
 
-def plot_damage_map_2d(damage_map, map_centers_x, map_centers_y, cmap='viridis', **damage_label_kwargs):
+def plot_damage_map_2d(damage_map, map_centers_x, map_centers_y, **damage_label_kwargs):
 
     # Make figure
     fig, ax = plt.subplots()
@@ -127,7 +144,7 @@ def plot_damage_map_2d(damage_map, map_centers_x, map_centers_y, cmap='viridis',
     
     extent = [map_centers_x[0] - bin_width_x/2., map_centers_x[-1] + bin_width_x/2., map_centers_y[-1] + bin_width_y/2., map_centers_y[0]- bin_width_y/2.]
 
-    im = ax.imshow(damage_map, extent=extent, cmap=cmap)
+    im = ax.imshow(damage_map, extent=extent)
 
     cbar = fig.colorbar(im)
 
@@ -136,7 +153,7 @@ def plot_damage_map_2d(damage_map, map_centers_x, map_centers_y, cmap='viridis',
 
     return fig, ax
 
-def plot_damage_map_contourf(damage_map, map_centers_x, map_centers_y, cmap='viridis', **damage_label_kwargs):
+def plot_damage_map_contourf(damage_map, map_centers_x, map_centers_y, **damage_label_kwargs):
 
     # Make figure
     fig, ax = plt.subplots()
@@ -145,7 +162,7 @@ def plot_damage_map_contourf(damage_map, map_centers_x, map_centers_y, cmap='vir
     mesh_x, mesh_y = np.meshgrid(map_centers_x, map_centers_y)
 
     # Plot contour
-    contour_2d = ax.contourf(mesh_x, mesh_y, damage_map, cmap=cmap)
+    contour_2d = ax.contourf(mesh_x, mesh_y, damage_map)
     _ = plt.clabel(contour_2d, inline=True, fmt='%1.2E', colors='k')
     ax.set_ylim(ax.get_ylim()[::-1])  # Inverty y axis in order to set origin to upper left
 
@@ -177,7 +194,7 @@ def plot_generic_fig(plot_data, fit_data=None, hist_data=None, fig_ax=None, **sp
     if fit_data:
         ax.plot(fit_data['xdata'], fit_data['func'](*fit_data['fit_args']), fit_data['fmt'], label=fit_data['label'], zorder=10)
     if hist_data:
-        if isinstance(hist_data['bins'], (int, str)):
+        if isinstance(hist_data['bins'], (int, str, type(None))):
             if hist_data['bins'] == 'stat':
                 n, s = np.mean(plot_data['xdata']), np.std(plot_data['xdata'])
                 binwidth = 6 * s / 100.
@@ -199,21 +216,103 @@ def plot_generic_fig(plot_data, fit_data=None, hist_data=None, fig_ax=None, **sp
     return fig, ax
 
 
+def plot_beam_current(timestamps, beam_current, ch_name=None, scan_data=None):
 
-def plot_beam_current_over_time(timestamps, beam_current, ch_name):
+    dtfts = datetime.fromtimestamp(timestamps[0])
 
-    fig, ax = plot_generic_fig(plot_data={'xdata': [datetime.fromtimestamp(ts) for ts in timestamps],
-                                          'ydata': beam_current,
-                                          'xlabel': 'Time',
-                                          'ylabel': f"Cup channel {ch_name} current / nA",
-                                          'label': f'{ch_name} current',
-                                          'title': f"Current of channel {ch_name}",
-                                          'fmt': 'C0.'})
+    plot_data = {'xdata': [datetime.fromtimestamp(ts) for ts in timestamps],
+                 'ydata': beam_current,
+                 'xlabel': f"{dtfts.strftime('%d %b %Y')}",
+                 'ylabel': f"Cup channel {ch_name} current / nA",
+                 'label': f"{ch_name or 'Beam'} current over {_calc_duration(start=timestamps[0], end=timestamps[-1], as_str=True)}",
+                 'title': f"Current of channel {ch_name}",
+                 'fmt': 'C0-'}
 
-    ax.xaxis.set_major_formatter(md.DateFormatter('%Y-%m-%d %H:%M'))
+    if ch_name is None:
+        plot_data['ylabel'] = 'Beam current / nA'
+        plot_data['title'] = "Beam current over time"
+
+    if scan_data is not None:
+        plot_data['title'] = 'Beam current during irradiation'
+
+    plot_data['label'] += ":\n    ({:.2f}{}{:.2f}) nA".format(beam_current.mean(), u'\u00b1', beam_current.std())
+
+    fig, ax = plot_generic_fig(plot_data=plot_data)
+
+    ax.xaxis.set_major_formatter(md.DateFormatter('%H:%M'))
     fig.autofmt_xdate()
 
     return fig, ax
+
+
+def plot_relative_beam_position(horizontal_pos, vertical_pos, n_bins=100, scan_data=None):
+
+    fig, ax_hist_2d = plt.subplots()
+
+    ax_hist_2d.set_aspect(1.0)
+
+    # Use fancy submodule, making grid of plots a 10000 times easier
+    # See https://matplotlib.org/stable/gallery/axes_grid1/scatter_hist_locatable_axes.html#sphx-glr-gallery-axes-grid1-scatter-hist-locatable-axes-py
+    anchor = make_axes_locatable(ax_hist_2d)
+
+    # Create a bunch of new axes for the other hists and the cbar
+    ax_hist_h = anchor.append_axes('top', 0.85, pad=0.1, sharex=ax_hist_2d)
+    ax_hist_v = anchor.append_axes('left', 0.85, pad=0.1, sharey=ax_hist_2d)
+    ax_cbar = anchor.append_axes('right', 0.2, pad=0.1)
+
+    # Set the tick locations correctly and inverty y axis of one hist to place it on left
+    ax_hist_h.xaxis.set_tick_params(labelbottom=False)
+    ax_hist_2d.yaxis.set_tick_params(labelleft=False)
+    ax_hist_v.invert_xaxis()
+
+    # Make the plots and trash what we don't need
+    _, _, _, im = ax_hist_2d.hist2d(horizontal_pos, vertical_pos, bins=(n_bins, n_bins), norm=LogNorm(), cmin=1)
+    _, _, _ = ax_hist_h.hist(horizontal_pos, bins=n_bins)
+    _, _, _ = ax_hist_v.hist(vertical_pos, bins=n_bins, orientation='horizontal')
+
+    # Add colorbar to predefined axis
+    fig.colorbar(im, cax=ax_cbar, label="")
+
+    # Add reticle
+    x_min, x_max = ax_hist_2d.get_xbound()
+    y_min, y_max = ax_hist_2d.get_ybound()
+    ax_hist_2d.annotate(text='Right', xy=(0, 0), xycoords='data',
+                        xytext=(x_max, 0),
+                        textcoords='data',
+                        arrowprops=dict(arrowstyle='<-', ls='--', alpha=0.8),
+                        ha='right', va='center', rotation=270)
+    ax_hist_2d.annotate(text='Left', xy=(0, 0), xycoords='data',
+                        xytext=(x_min, 0),
+                        textcoords='data',
+                        arrowprops=dict(arrowstyle='<-', ls='--', alpha=0.8),
+                        ha='left', va='center', rotation=90)
+    ax_hist_2d.annotate(text='Up', xy=(0, 0), xycoords='data',
+                        xytext=(0, y_max),
+                        textcoords='data',
+                        arrowprops=dict(arrowstyle='<-', ls='--', alpha=0.8),
+                        ha='center', va='top')
+    ax_hist_2d.annotate(text='Down', xy=(0, 0), xycoords='data',
+                        xytext=(0, y_min),
+                        textcoords='data',
+                        arrowprops=dict(arrowstyle='<-', ls='--', alpha=0.8),
+                        ha='center', va='bottom')
+
+    # Make labels and legends
+    ax_hist_h.set_title('Beam-mean position relative to center')
+    ax_hist_2d.set_xlabel('Rel. horizontal deviation / %')
+    ax_hist_v.set_ylabel('Rel. vertical deviation / %')
+
+    # Fake a little legend
+    ax_hist_h.text(s=r'$\mathrm{\mu_h}$='+"({:.1f}{}{:.1f}) %".format(horizontal_pos.mean(), u'\u00b1', horizontal_pos.std()),
+                   x=x_min*0.95,
+                   y=ax_hist_h.get_ylim()[-1]*0.775, rotation=0, fontsize=10,
+                   bbox=dict(boxstyle='round', facecolor='white', edgecolor='grey', alpha=0.33))
+    ax_hist_v.text(s=r'$\mathrm{\mu_v}$='+"({:.1f}{}{:.1f}) %".format(vertical_pos.mean(), u'\u00b1', vertical_pos.std()),
+                   x=ax_hist_v.get_xlim()[0]*0.925,
+                   y=y_min*0.925, rotation=90, fontsize=10,
+                   bbox=dict(boxstyle='round', facecolor='white', edgecolor='grey', alpha=0.33))
+
+    return fig, (ax_hist_2d, ax_hist_h, ax_hist_v)
 
 
 def plot_calibration(calib_data, ref_data, calib_sig, ref_sig, red_chi, beta_lambda, hist=False):
@@ -238,7 +337,7 @@ def plot_calibration(calib_data, ref_data, calib_sig, ref_sig, red_chi, beta_lam
                                          'fit_args': [[beta_const.n], calib_data],
                                          'fmt': 'C1-',
                                          'label': fit_label},
-                               hist_data={'bins': (100, 100), 'cmap': 'viridis', 'norm': LogNorm()} if hist else {})
+                               hist_data={'bins': (100, 100), 'norm': LogNorm()} if hist else {})
 
     # Make figure and axis
     _, _ = ax.set_ylim(0, np.max(ref_data) * (1.25))
@@ -365,92 +464,3 @@ def plot_everything(data, **kwargs):
     neqax.grid(axis='y', zorder=11)
     beamax.grid(axis='x', zorder=10)
     return fig
-    
-#******* Beamplotting ********#
-def plot_beam_current(timestamps, beam_currents, while_scan=None):
-    tss = datetime.fromtimestamp(timestamps[0])
-    day = tss.strftime("%d")
-    month = tss.strftime("%B")
-    year = tss.strftime("%Y")
-    fig_title = "Beam current over time" if while_scan is None else "Beam current over time during scan"
-    fig_label = "Beam"
-    xlabel = "Time on {} {} {}".format(day, month, year)
-    fig, ax = plot_generic_fig(plot_data={'xdata': [datetime.fromtimestamp(ts) for ts in timestamps],
-                                          'ydata': beam_currents,
-                                          'xlabel': xlabel,
-                                          'ylabel': f"Beam current / nA",
-                                          'label': fig_label,
-                                          'title': fig_title,
-                                          'fmt': '-C0'})
-
-    ax.xaxis.set_major_formatter(md.DateFormatter('%H:%M'))
-    fig.autofmt_xdate()
-    return fig, ax
-
-def plot_beam_current_hist(beam_currents, start, end, while_scan=None):
-    tss = datetime.fromtimestamp(start)
-    tse = datetime.fromtimestamp(end)
-    tdiff = tse-tss
-    tdiff = tdiff.total_seconds()
-    tdiffh = (tdiff-tdiff%3600)/3600
-    tdiff = tdiff%3600
-    tdiffm = (tdiff-tdiff%60)/60
-
-    fig_label = "Mean beam current over {} hours, {} mins.:\n({:.0f}±{:.0f}) nA".format(int(tdiffh), int(tdiffm), np.mean(beam_currents), np.std(beam_currents))
-    fig_title = "Beam-Current Distribution" if while_scan is None else "Beam-current distribution during scan"
-    fig, ax = plot_generic_fig(plot_data={'xdata': beam_currents,
-                                          'xlabel': f"Beam current / nA",
-                                          'ylabel': f"#",
-                                          'label': fig_label,
-                                          'title': fig_title,
-                                          'fmt': 'C0'},
-                               hist_data={'bins': 'stat'})
-    
-    return fig, ax
-
-def plot_beam_deviation(horizontal_deviation, vertical_deviation, while_scan=None):
-    fig = plt.figure()
-    gs = fig.add_gridspec(2, 3,  width_ratios=(7, 2, 0.5), height_ratios=(2, 7),
-                        left=0.1, right=0.9, bottom=0.1, top=0.9,
-                        wspace=0.05, hspace=0.05)
-    ax = fig.add_subplot(gs[1, 0])
-    ax_histx = fig.add_subplot(gs[0, 0], sharex=ax)
-    ax_histy = fig.add_subplot(gs[1, 1], sharey=ax)
-    ax_cbar = fig.add_subplot(gs[1, 2])
-    ax_cbar.spines["left"].set_visible(False)
-    ax_cbar.spines["right"].set_visible(False)
-    ax_cbar.spines["top"].set_visible(False)
-    ax_cbar.spines["bottom"].set_visible(False)
-    ax_cbar.set_yticks([])
-    ax_cbar.set_xticks([])
-
-    
-    ax_histy.tick_params(top=True, labeltop=True, bottom=False, labelbottom=False)
-    fig_title = "Relative beam-distribution from mean position" if while_scan is None \
-        else "Relative beam-distribution from mean position while scanning"
-    fig.suptitle(fig_title)
-    ax.set_xlabel("Horizontal / % ")
-    ax.set_ylabel("Vertical / %")
-    n_bins = 100 #bins of 2% each
-    xmean = np.mean(vertical_deviation)
-    xstd = np.std(vertical_deviation)
-    ymean = np.mean(horizontal_deviation)
-    ystd = np.std(horizontal_deviation)
-    xlabel = '\t'+ r'Horizontal: $(%.2f\pm%.2f)$' % (xmean, xstd) + r'$\%$' + '\t'
-    ylabel =  '\t'+r'Vertical: $(%.2f\pm%.2f)$' % (ymean, ystd) + r'$\%$' + '\t'
-    _, _, _, im = ax.hist2d(horizontal_deviation, vertical_deviation, bins=(n_bins, n_bins),norm=LogNorm(), cmap='viridis', cmin=1)
-    plt.colorbar(mappable=im, ax=ax_cbar, fraction=1., pad=0.)
-    _, _, _ = ax_histx.hist(vertical_deviation, bins=n_bins)
-    _, _, _ = ax_histy.hist(horizontal_deviation, bins=n_bins, orientation='horizontal')
-    ax_histx.set_yticks(ax_histy.get_xticks())
-    ax.plot([],[],ls='none',label=xlabel)
-    p1, = ax.plot([], [], ls='none')
-    leg = ax.legend((p1,p1),(xlabel,ylabel),loc='lower center')
-    ax.add_artist(leg)
-    ax.grid()
-    ax_histx.grid()
-    ax_histy.grid()
-    ax_histx.tick_params(axis="x", labelbottom=False)
-    ax_histy.tick_params(axis="y", labelleft=False)
-    
-    return fig, ax
