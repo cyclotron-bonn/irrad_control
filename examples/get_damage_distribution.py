78b861cd454c30638c98e1353ec33190da6ab566
leloup314 <wolf@physik.uni-bonn.de>
2023-10-12 16:48:40 +0200

    ENH: extend example

--- examples/get_damage_distribution.py
+++ examples/get_damage_distribution.py
@@ -29,27 +29,58 @@ if __name__ == '__main__':
 
     # Get the config of our irrad_server; use fancy ',' extraction syntax
     server_config, = (conf for serv, conf in config['server'].items() if conf['name'] == irrad_server)
+
+
+    ##############################################################################################################
+    #            Generate fluence distribution on scan area and extract the DUT region from it                   #
+    ##############################################################################################################
+
     
-    # Generate the primary fluence distribution from the data
+    # Generate the primary fluence distribution of scan area from the data
     # Returns tuple of (fluence_map, error_map, bin_centers_x, bin_centers_y)
     fluence_map, error_map, bin_c_x, bin_c_y = fluence.generate_fluence_map(beam_data=data[irrad_server]['Beam'],
                                                                             scan_data=data[irrad_server]['Scan'],
                                                                             irrad_data=data[irrad_server]['Irrad'],
                                                                             bins=(100, 100))
+    
+    # Extract DUT map
+    fluence_map_DUT, bin_c_x_DUT, bin_c_y_DUT = fluence.extract_dut_map(fluence_map=fluence_map,
+                                                                        map_bin_centers_x=bin_c_x,
+                                                                        map_bin_centers_y=bin_c_y,
+                                                                        irrad_data=data[irrad_server]['Irrad'])
 
-    # Print 2D primary fluence (e.g. proton fluence) map
-    print(f"Proton fluence map [p/cm²] ({fluence_map.shape}):\n\n{fluence_map}")
+    # Extract respective dut error map
+    error_map_DUT, _, _ = fluence.extract_dut_map(fluence_map=error_map,
+                                                  map_bin_centers_x=bin_c_x,
+                                                  map_bin_centers_y=bin_c_y,
+                                                  irrad_data=data[irrad_server]['Irrad'])
+    
 
+    ##############################################################################################################
+    #           Calculate the NEQ and TID distributions and their errors from the DUT maps                       #
+    ##############################################################################################################
+    
+    
     # Convert to NEQ fluence and error via Gaussian error prop.
-    neq_map = fluence_map * server_config['daq']['kappa']['nominal']
-    neq_error_map = ((server_config['daq']['kappa']['nominal'] * error_map)**2 + (neq_map * server_config['daq']['kappa']['sigma'])**2)**.5
+    neq_map_DUT = fluence_map_DUT * server_config['daq']['kappa']['nominal']
+    neq_error_map_DUT = ((server_config['daq']['kappa']['nominal'] * error_map_DUT)**2 + (neq_map_DUT * server_config['daq']['kappa']['sigma'])**2)**.5
 
-    # Print 2D NEQ fluence map
-    print(f"NEQ fluence map [neq/cm²] ({neq_map.shape}):\n\n{neq_map}")
 
     # Convert to TID and error via Gaussian error prop.
-    tid_map = formulas.tid_per_scan(primary_fluence=fluence_map, stopping_power=server_config['daq']['stopping_power'])
-    tid_error_map = formulas.tid_per_scan(primary_fluence=error_map, stopping_power=server_config['daq']['stopping_power'])
+    tid_map_DUT = formulas.tid_per_scan(primary_fluence=fluence_map_DUT, stopping_power=server_config['daq']['stopping_power'])
+    tid_error_map_DUT = formulas.tid_per_scan(primary_fluence=error_map_DUT, stopping_power=server_config['daq']['stopping_power'])
+    
+    
+    ##############################################################################################################
+    #                                     Print the different results                                            #
+    ##############################################################################################################
 
-    # Print 2D NEQ fluence map
-    print(f"TID map [Mrad] ({tid_map.shape}):\n\n{tid_map}")
+
+    # Print 2D primary fluence (e.g. proton fluence) map on scan area
+    print(f"Proton fluence map on scan area [p/cm²] (bins={fluence_map.shape}):\n\n{fluence_map}\n\n")
+    
+    # Print 2D NEQ fluence map on DUT
+    print(f"NEQ fluence map on DUT [neq/cm²] (bins={neq_map_DUT.shape}, mean={neq_map_DUT.mean():.2E}):\n\n{neq_map_DUT}\n\n")
+    
+    # Print 2D TID map on DUT
+    print(f"TID map on DUT [Mrad] (bins={tid_map_DUT.shape}, mean={tid_map_DUT.mean():.2E}):\n\n{tid_map_DUT}\n\n")
