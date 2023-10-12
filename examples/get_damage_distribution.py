5598882771df7bd1064659d7b4e1b0e771670170
leloup314 <wolf@physik.uni-bonn.de>
2023-10-12 15:48:35 +0200

    ENH: add examples

---
+++ examples/get_damage_distribution.py
@@ -0,0 +1,49 @@
+# This file demonstrates how to get the 2D damage distribution on the scan area as well as the DUT.
+# Furthermore, the conversion to NIEL and TID damage is shown.
+
+from irrad_control.analysis import fluence, utils, formulas
+
+# Path to data file
+data_file = '../tests/fixtures/test_irrad_w_corr.h5'
+
+# Path to config file
+config_file = '../tests/fixtures/test_irrad_w_corr.yaml'
+
+# Name of the irradiation server used e.g. 'HSR' for 'Hochstromraum'
+irrad_server = 'HSR'
+
+
+if __name__ == '__main__':
+
+    # Returns a tuple of (data, config)
+    data, config = utils.load_irrad_data(data_file=data_file,
+                                         config_file=config_file,
+                                         specify_entries=['Beam', 'Scan', 'Irrad'],  # Here, you can place a list of entries to load e.g. ['Beam', 'Damage', Raw', Irrad']. If None, load everything
+                                  )
+
+    # Get the config of our irrad_server; use fancy ',' extraction syntax
+    server_config, = (conf for serv, conf in config['server'].items() if conf['name'] == irrad_server)
+    
+    # Generate the primary fluence distribution from the data
+    # Returns tuple of (fluence_map, error_map, bin_centers_x, bin_centers_y)
+    fluence_map, error_map, bin_c_x, bin_c_y = fluence.generate_fluence_map(beam_data=data[irrad_server]['Beam'],
+                                                                            scan_data=data[irrad_server]['Scan'],
+                                                                            irrad_data=data[irrad_server]['Irrad'],
+                                                                            bins=(100, 100))
+
+    # Print 2D primary fluence (e.g. proton fluence) map
+    print(f"Proton fluence map [p/cm²] ({fluence_map.shape}):\n\n{fluence_map}")
+
+    # Convert to NEQ fluence and error via Gaussian error prop.
+    neq_map = fluence_map * server_config['daq']['kappa']['nominal']
+    neq_error_map = ((server_config['daq']['kappa']['nominal'] * error_map)**2 + (neq_map * server_config['daq']['kappa']['sigma'])**2)**.5
+
+    # Print 2D NEQ fluence map
+    print(f"NEQ fluence map [neq/cm²] ({neq_map.shape}):\n\n{neq_map}")
+
+    # Convert to TID and error via Gaussian error prop.
+    tid_map = formulas.tid_per_scan(primary_fluence=fluence_map, stopping_power=server_config['daq']['stopping_power'])
+    tid_error_map = formulas.tid_per_scan(primary_fluence=error_map, stopping_power=server_config['daq']['stopping_power'])
+
+    # Print 2D NEQ fluence map
+    print(f"TID map [Mrad] ({tid_map.shape}):\n\n{tid_map}")
---
+++ examples/read_irradiation_data.py
@@ -0,0 +1,41 @@
+# This file shows how to read data poduced by irrad_control
+# Please check https://github.com/cyclotron-bonn/irrad_control/blob/main/irrad_control/analysis/dtype.py
+# for info on the available data types for each entry.
+
+# Utility function for loading data
+from irrad_control.analysis.utils import load_irrad_data
+
+# Path to data file
+data_file = '../tests/fixtures/test_irrad_w_corr.h5'
+
+# Path to config file
+config_file = '../tests/fixtures/test_irrad_w_corr.yaml'
+
+# Name of the irradiation server used e.g. 'HSR' for 'Hochstromraum'
+irrad_server = 'HSR'
+
+if __name__ == '__main__':
+    
+    # Returns a tuple of (data, config)
+    data, config = load_irrad_data(data_file=data_file,
+                                   config_file=config_file,
+                                   specify_entries=None,  # Here, you can place a list of entries to load e.g. ['Beam', 'Damage', Raw', Irrad']. If None, load everything
+                                  )
+
+    # *data* is a dict with the respective irradiation servers as keys
+    actual_data = data[irrad_server]
+
+    # Available data
+    print(f"Available entries: {[n for n in actual_data]}")
+
+    # Beam-related data; numpy structured array
+    beam_data = actual_data['Beam']
+
+    # Get timestamps and beam currents
+    ts_beam, current_beam = beam_data['timestamp'], beam_data['beam_current']
+
+    # Get temperature data; temperature data can be recorded using different device therefore there is the *ArduinoNTCReadout* in the dict path
+    ts_temp = actual_data['Temperature']['ArduinoNTCReadout']['timestamp']
+    temp_DUT =  actual_data['Temperature']['ArduinoNTCReadout']['DUT']
+
+    print(f"Average DUT temperature: {temp_DUT.mean():.2f} °C")
