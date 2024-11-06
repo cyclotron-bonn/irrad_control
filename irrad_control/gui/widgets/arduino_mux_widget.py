414573326ddc4681a6ccddd328e09d6e6d4068ee
Leoni <leoni.dessau@mailbox.org>
2024-11-06 19:43:14 +0100

    basic renaming has beeen added

--- irrad_control/gui/widgets/arduino_mux_widget.py
+++ irrad_control/gui/widgets/arduino_mux_widget.py
@@ -1,3 +1,4 @@
+from os import rename
 from PyQt5 import QtWidgets, QtCore
 from collections import defaultdict
 
@@ -26,11 +27,14 @@ class ArduinoMuxWidget(ControlWidget):
         pass
 
     def activate_transmit(self):
-        self.transmit_state_button.setEnabled(True)
+        button_states = [b.isChecked() for b in self.channel_boxes]
+        if self.internal_state != button_states :
+            self.transmit_state_button.setEnabled(True)
+        else:
+            self.transmit_state_button.setEnabled(False)
 
 
     def _init_buttons(self):
-#        channel_boxes = [QtWidgets.QCheckBox('channel ' + str(n)) for n in range(1, 17)]
         self.channel_boxes = [QtWidgets.QPushButton('channel ' + str(n)) for n in range(1, 17)]
 
         style = """QPushButton {
@@ -42,21 +46,20 @@ class ArduinoMuxWidget(ControlWidget):
 
         for i in range(len(self.channel_boxes)):
             self.channel_boxes[i].setCheckable(True)
-            self.channel_boxes[i].setFixedSize(80, 60)
+            self.channel_boxes[i].setFixedSize(100, 60)
             self.channel_boxes[i].setStyleSheet(style)
             self.channel_boxes[i].clicked.connect(self.activate_transmit)
 
         label_con1 = QtWidgets.QLineEdit('Connector 1')
         label_con1.setAlignment(QtCore.Qt.AlignCenter)
         label_con1.setReadOnly(True)
+
         label_con2 = QtWidgets.QLineEdit('Connector 2')
         label_con2.setAlignment(QtCore.Qt.AlignCenter)
         label_con2.setReadOnly(True)
 
         con1_box = QtWidgets.QGridLayout()
-        #con1_box.setSpacing(2)
         con2_box = QtWidgets.QGridLayout()
-        #con2_box.setSpacing(2)
 
         con1_box.addWidget(label_con1, 0, 0, 1, 2)
         con2_box.addWidget(label_con2, 0, 0, 1, 2)
@@ -75,12 +78,31 @@ class ArduinoMuxWidget(ControlWidget):
         meta_box.addLayout(con1_box)
         meta_box.addLayout(con2_box)
 
+        rename_button = QtWidgets.QPushButton('rename channels')
+        rename_button.clicked.connect(self.input_dialog)
+
         self.add_widget(meta_box)
         self.add_widget(self.transmit_state_button)
+        self.add_widget(rename_button)
 
+
+    def rename_channel_buttons(self, naming_string):
+        for line in naming_string.split('\n'):
+            num = int(line.split(":")[0])
+            name = "".join(line.split(":")[1:])
+            self.channel_boxes[num - 1].setText(name)
+
+
+    def input_dialog(self):
+        filename , ok = QtWidgets.QFileDialog.getOpenFileName(self, 'InputDialog','channel: name')
+        if ok:
+            with open(filename) as file:
+                text = file.read()
+                self.rename_channel_buttons(text)
 
     def transmit_state(self, check_boxes):
         self.transmit_state_button.setEnabled(False)
+        self.internal_state = [b.isChecked() for b in self.channel_boxes]
         for i in range(len(check_boxes)):
             print(str(i) + ' ' + str(check_boxes[i].isChecked()))
             self.set_channel(i, check_boxes[i].isChecked())
