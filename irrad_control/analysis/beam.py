e00542b5fa55bd1796cb2f775901a7b6f24c54a5
leloup314 <pascal.wolf@posteo.de>
2025-01-26 18:54:43 +0100

    ENH: adapt analysis scripts to genrate summary information

--- irrad_control/analysis/beam.py
+++ irrad_control/analysis/beam.py
@@ -1,13 +1,20 @@
 from irrad_control.analysis import plotting, constants
-from irrad_control.utils.utils import duration_str_from_secs
+from irrad_control.analysis.utils import duration_str_from_secs
 from numpy import nanmean, nanstd
 
-def main(data, config=None):
+def main(data, config, summary=None):
 
     figs = []
     server = config['name']
 
     beam_current = data[server]['Beam']['beam_current'] / constants.nano
+
+    summary['beam']['current'] = nanmean(beam_current)
+    summary['beam']['ion'] = config['daq']['ion']
+    summary['beam']['energy'] = config['daq']['ekin']
+    summary['beam']['lambda'] = config['daq']['lambda']['nominal']
+    summary['beam']['kappa'] = None if 'kappa' not in config['daq'] else config['daq']['kappa']['nominal']
+    summary['summary_generated'] = True
 
     # Beam current over time
     fig, _ = plotting.plot_beam_current(timestamps=data[server]['Beam']['timestamp'],
