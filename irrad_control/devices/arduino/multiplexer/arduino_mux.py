a75a81549e0abbcdb22918172a87b51ff84f28ae
leloup314 <pascal.wolf@posteo.de>
2025-11-07 16:57:21 +0100

    MAINT: refactor Arduino MUX code

--- irrad_control/devices/arduino/multiplexer/arduino_mux.py
+++ irrad_control/devices/arduino/multiplexer/arduino_mux.py
@@ -1,9 +1,11 @@
+from threading import Event
 from irrad_control.devices.arduino.arduino_serial import ArduinoSerial
-import threading
-import time
-import logging
+from irrad_control.utils.worker import ThreadWorker
+
 
 class ArduinoMUX(ArduinoSerial):
+    # FIXME: this ping_loop implementation does not look threadsafe w.r.t en/disable_channel
+
     CMDS = {
         'enable_channel': 'E',
         'disable_channel': 'D',
@@ -20,45 +22,29 @@ class ArduinoMUX(ArduinoSerial):
 
 
     def __init__(self, port="/dev/ttyS0", baudrate=9600, timeout=1):
-        #logging.error("initiating arduino mux")
-        #logging.error("port={} baudrate={} timeout={}".format(port, baudrate, timeout))
         super().__init__(port=port, baudrate=baudrate, timeout=timeout)
-        #logging.error("super init finished")
-        # start ping thread here??
-        self.enable_ping = True
-        self.ping_thread = threading.Thread(target = self.ping_loop)
+        
+        self.stop_ping = Event()
+        self.ping_thread = ThreadWorker(target=self.ping_loop)
         self.ping_thread.start()
 
-
     def ping_loop(self):
-        while self.enable_ping:
+        while not self.stop_ping.wait(self.delay):
             self.ping()
-            time.sleep(self.delay)
-
 
     def ping(self):
-        #logging.error("pinging")
         self.write(self.create_command(self.CMDS['ping']))
 
-
-    def _enable_channel(self, channel: int = 16):
-        #logging.error("called enable channel")
-        #logging.error("{}".format(self.create_command(self.CMDS['enable_channel'], channel)))
+    def enable_channel(self, channel: int = 16):
         self.write(self.create_command(self.CMDS['enable_channel'], channel))
 
-
-    def _disable_channel(self, channel: int = 16):
-        #logging.error("called disable channel")
-        #logging.error("{}".format(self.create_command(self.CMDS['disable_channel'], channel)))
+    def disable_channel(self, channel: int = 16):
         self.write(self.create_command(self.CMDS['disable_channel'], channel))
 
-
-    #@property
     def channel_states(self):
         response = self.query(self.create_command(self.CMDS['get_status']))
         response = response.split()
 
-
     def shutdown(self):
-        self.enable_ping = False
+        self.stop_ping.set()
         self.ping_thread.join()
